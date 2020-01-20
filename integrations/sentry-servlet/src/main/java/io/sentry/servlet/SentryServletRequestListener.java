5ff07ab733e435710ee82b91342a4f51da4691dd
Lukas Krejci <lkrejci@redhat.com>
2020-01-20 01:12:27 +0100

    Attempt at SentryServletRequestListener.

--- integrations/sentry-servlet/src/main/java/io/sentry/servlet/SentryServletRequestListener.java
+++ integrations/sentry-servlet/src/main/java/io/sentry/servlet/SentryServletRequestListener.java
@@ -1,53 +1,89 @@
 package io.sentry.servlet;
 
-import io.sentry.core.IHub;
-import io.sentry.core.Integration;
 import io.sentry.core.Sentry;
-import io.sentry.SentryClient;
+import io.sentry.core.protocol.Request;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletRequestEvent;
 import javax.servlet.ServletRequestListener;
 import javax.servlet.http.HttpServletRequest;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
- * Request listener in charge of capturing {@link HttpServletRequest} to allow {@link
- * io.sentry.event.helper.HttpEventBuilderHelper} to provide details on the current HTTP session in
- * the event sent to Sentry.
+ * This request listener pushes a new scope into sentry that enriches a Sentry event with
+ * the details about the current request upon sending.
  */
 public class SentryServletRequestListener implements ServletRequestListener {
-  private static final ThreadLocal<HttpServletRequest> THREAD_REQUEST = new ThreadLocal<>();
-
-  public static HttpServletRequest getServletRequest() {
-    return THREAD_REQUEST.get();
-  }
-
   @Override
   public void requestDestroyed(ServletRequestEvent servletRequestEvent) {
-    THREAD_REQUEST.remove();
-
-    try {
-      Sentry.clearBreadcrumbs();
-    } catch (RuntimeException e) {
-      logger.error("Error clearing Context state.", e);
-    }
+    Sentry.clearBreadcrumbs();
   }
 
   @Override
   public void requestInitialized(ServletRequestEvent servletRequestEvent) {
-    ServletRequest servletRequest = servletRequestEvent.getServletRequest();
-    if (servletRequest instanceof HttpServletRequest) {
-      THREAD_REQUEST.set((HttpServletRequest) servletRequest);
-    }
+    Sentry.pushScope();
+    Sentry.configureScope(scope -> scope.addEventProcessor((event, __) -> {
+      ServletRequest sreq = servletRequestEvent.getServletRequest();
+      if (!(sreq instanceof HttpServletRequest)) {
+        return event;
+      }
+
+      if (event.getRequest() != null) {
+        // the application code already set the request - don't overwrite it then
+        return event;
+      }
+      
+      HttpServletRequest httpReq = (HttpServletRequest) sreq;
+
+      Request req = new Request();
+      req.setCookies(cookiesToString(httpReq.getHeaders("Cookie")));
+      req.setHeaders(allHeaders(httpReq));
+      req.setMethod(httpReq.getMethod());
+      req.setQueryString(httpReq.getQueryString());
+      req.setUrl(httpReq.getRequestURI());
+      // TODO add request attributes as "envs" of the sentry request or some other attr?
+
+      event.setRequest(req);
+      return event;
+    }));
   }
 
-  /**
-   * Used for resetting the state of the thread-local storing the current HttpServletRequest.
-   *
-   * <p>This method exists solely for testing purposes.
-   */
-  static void reset() {
-    THREAD_REQUEST.remove();
+  private static String cookiesToString(Enumeration<String> cookies) {
+    if (cookies == null) {
+      return null;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    while (cookies.hasMoreElements()) {
+      sb.append(cookies.nextElement());
+      sb.append("; ");
+    }
+
+    if (sb.length() > 2) {
+      sb.setLength(sb.length() - 2);
+    }
+
+    return sb.toString();
+  }
+
+  private static Map<String, String> allHeaders(HttpServletRequest req) {
+    Map<String, String> headers = new HashMap<>();
+    Enumeration<String> names = req.getHeaderNames();
+    StringBuilder sb = new StringBuilder();
+    while (names.hasMoreElements()) {
+      sb.setLength(0);
+      String name = names.nextElement();
+      Enumeration<String> values = req.getHeaders(name);
+      while (values.hasMoreElements()) {
+        sb.append(values.nextElement()).append(",");
+      }
+      if (sb.length() > 0) {
+        sb.setLength(sb.length() - 1);
+      }
+      headers.put(name, sb.toString());
+    }
+
+    return headers;
   }
 }
