665c9521df416431458a2e1ed96ceb4b718b2790
Lukas Krejci <lkrejci@redhat.com>
2020-04-05 00:23:21 +0200

    Implement Log4j integration.

--- integrations/sentry-log4j/build.gradle.kts
+++ integrations/sentry-log4j/build.gradle.kts
@@ -1,11 +1,14 @@
 plugins {
     `java-library`
+    kotlin("jvm")
 }
 
 dependencies {
+    compileOnly(Config.CompileOnly.jetbrainsAnnotations)
     implementation(Config.Libs.log4j)
     implementation(project(":sentry-core"))
     testImplementation(Config.TestLibs.kotlinTestJunit)
+    testImplementation(Config.TestLibs.mockitoKotlin)
 }
 
 configure<JavaPluginConvention> {
--- integrations/sentry-log4j/src/main/java/io/sentry/log4j/SentryAppender.java
+++ integrations/sentry-log4j/src/main/java/io/sentry/log4j/SentryAppender.java
@@ -1,18 +1,27 @@
 package io.sentry.log4j;
 
+import io.sentry.core.Sentry;
 import io.sentry.core.SentryEvent;
 import io.sentry.core.SentryLevel;
+import io.sentry.core.protocol.Message;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.Level;
+import org.apache.log4j.spi.ErrorCode;
 import org.apache.log4j.spi.Filter;
-import org.apache.log4j.spi.LocationInfo;
 import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.spi.ThrowableInformation;
 
+import java.util.Date;
+
+/**
+ * This is a Log4j appender that will send the log messages to Sentry.
+ */
 public class SentryAppender extends AppenderSkeleton {
-  /** Name of the {@link SentryEvent#extra} property containing NDC details. */
-  public static final String LOG4J_NDC = "log4J-NDC";
-  /** Name of the {@link SentryEvent#extra} property containing the Thread name. */
-  public static final String THREAD_NAME = "Sentry-Threadname";
+  /** Name of the {@link SentryEvent#setExtra(String, Object)} property containing NDC details. */
+  public static final String LOG4J_NDC = "log4j-NDC";
+
+  /** Tells whether we're currently capturing a log statement or not */
+  private final ThreadLocal<Boolean> capturing = ThreadLocal.withInitial(() -> false);
 
   /** Creates an instance of SentryAppender. */
   public SentryAppender() {
@@ -37,119 +46,75 @@ public class SentryAppender extends AppenderSkeleton {
     } else if (level.isGreaterOrEqual(Level.ALL)) {
       return SentryLevel.DEBUG;
     } else {
-      return null;
+      return SentryLevel.LOG;
     }
-  }
-
-  /**
-   * Transforms the location info of a log into a stacktrace element (stackframe).
-   *
-   * @param location details on the location of the log.
-   * @return a stackframe.
-   */
-  protected static StackTraceElement asStackTraceElement(LocationInfo location) {
-    String fileName =
-        (LocationInfo.NA.equals(location.getFileName())) ? null : location.getFileName();
-    int line =
-        (LocationInfo.NA.equals(location.getLineNumber()))
-            ? -1
-            : Integer.parseInt(location.getLineNumber());
-    return new StackTraceElement(location.getClassName(), location.getMethodName(), fileName, line);
   }
 
   @Override
   protected void append(LoggingEvent loggingEvent) {
-    //    // Do not log the event if the current thread is managed by sentry
-    //    if (SentryEnvironment.isManagingThread()) {
-    //      return;
-    //    }
-    //
-    //    SentryEnvironment.startManagingThread();
-    //    try {
-    //      EventBuilder eventBuilder = createEventBuilder(loggingEvent);
-    //      Sentry.capture(eventBuilder);
-    //    } catch (RuntimeException e) {
-    //      getErrorHandler()
-    //          .error(
-    //              "An exception occurred while creating a new event in Sentry",
-    //              e,
-    //              ErrorCode.WRITE_FAILURE);
-    //    } finally {
-    //      SentryEnvironment.stopManagingThread();
-    //    }
+    if (!Sentry.isEnabled()) {
+      return;
+    }
+
+    // Do not log the event if we're in the middle of sentry logging using this logger. We would
+    // most probably end up in an infinite loop.
+    if (capturing.get()) {
+      System.err.println("Sentry: The logger configured in Sentry options writes to the Sentry Log4j appender. This" +
+              " is illegal because we would now most probably enter an infinite loop. Please use a logger with" +
+              " a different appender for this. The message was: " +
+              loggingEvent.getRenderedMessage());
+      return;
+    }
+
+    capturing.set(true);
+    try {
+      Sentry.captureEvent(createEvent(loggingEvent));
+    } catch (RuntimeException e) {
+      getErrorHandler()
+          .error(
+              "An exception occurred while creating a new event in Sentry",
+              e,
+              ErrorCode.WRITE_FAILURE);
+    } finally {
+      capturing.set(false);
+    }
   }
 
-  //  /**
-  //   * Builds an EventBuilder based on the logging event.
-  //   *
-  //   * @param loggingEvent Log generated.
-  //   * @return EventBuilder containing details provided by the logging system.
-  //   */
-  //  protected EventBuilder createEventBuilder(LoggingEvent loggingEvent) {
-  //    EventBuilder eventBuilder =
-  //        new EventBuilder()
-  //            .withSdkIntegration("log4j")
-  //            .withTimestamp(new Date(loggingEvent.getTimeStamp()))
-  //            .withMessage(loggingEvent.getRenderedMessage())
-  //            .withLogger(loggingEvent.getLoggerName())
-  //            .withLevel(formatLevel(loggingEvent.getLevel()))
-  //            .withExtra(THREAD_NAME, loggingEvent.getThreadName());
-  //
-  //    ThrowableInformation throwableInformation = null;
-  //    try {
-  //      throwableInformation = loggingEvent.getThrowableInformation();
-  //    } catch (NullPointerException expected) {
-  //      // `throwableInformation` is already set.
-  //    }
-  //
-  //    if (throwableInformation != null) {
-  //      Throwable throwable = throwableInformation.getThrowable();
-  //      eventBuilder.withSentryInterface(new ExceptionInterface(throwable));
-  //    } else if (loggingEvent.getLocationInformation().fullInfo != null) {
-  //      LocationInfo location = loggingEvent.getLocationInformation();
-  //      if (!LocationInfo.NA.equals(location.getFileName())
-  //          && !LocationInfo.NA.equals(location.getLineNumber())) {
-  //        StackTraceElement[] stackTrace = {asStackTraceElement(location)};
-  //        eventBuilder.withSentryInterface(new StackTraceInterface(stackTrace));
-  //      }
-  //    }
-  //
-  //    if (loggingEvent.getNDC() != null) {
-  //      eventBuilder.withExtra(LOG4J_NDC, loggingEvent.getNDC());
-  //    }
-  //
-  //    Set<String> extraTags = Sentry.getStoredClient().getMdcTags();
-  //    @SuppressWarnings("unchecked")
-  //    Map<String, Object> properties = (Map<String, Object>) loggingEvent.getProperties();
-  //    for (Map.Entry<String, Object> mdcEntry : properties.entrySet()) {
-  //      if (extraTags.contains(mdcEntry.getKey())) {
-  //        eventBuilder.withTag(mdcEntry.getKey(), mdcEntry.getValue().toString());
-  //      } else {
-  //        eventBuilder.withExtra(mdcEntry.getKey(), mdcEntry.getValue());
-  //      }
-  //    }
-  //
-  //    return eventBuilder;
-  //  }
+    /**
+     * Builds an EventBuilder based on the logging event.
+     *
+     * @param loggingEvent Log generated.
+     * @return EventBuilder containing details provided by the logging system.
+     */
+    protected SentryEvent createEvent(LoggingEvent loggingEvent) {
+      SentryEvent event = new SentryEvent(new Date(loggingEvent.getTimeStamp()));
+      Message message = new Message();
+      message.setFormatted(loggingEvent.getRenderedMessage());
+      event.setMessage(message);
+      event.setLogger(loggingEvent.getLoggerName());
+      event.setLevel(formatLevel(loggingEvent.getLevel()));
+
+      ThrowableInformation throwableInformation = loggingEvent.getThrowableInformation();
+      if (throwableInformation != null) {
+        event.setThrowable(throwableInformation.getThrowable());
+      }
+
+      // We could use the loggingEvent.getLocationInformation() to get the location information even for just a simple
+      // log message.
+      // I am not sure if that is a right thing to do though, because a) users probably don't expect that info because
+      // it's not usually present in the log files either and b) location info in the logging event is potentially lazy
+      // initialized to the location of the current callsite (e.g. here) which is not what the user would expect.
+
+      if (loggingEvent.getNDC() != null) {
+        event.setExtra(LOG4J_NDC, loggingEvent.getNDC());
+      }
+
+      return event;
+    }
 
   @Override
   public void close() {
-    //    SentryEnvironment.startManagingThread();
-    //    try {
-    //      if (this.closed) {
-    //        return;
-    //      }
-    //      this.closed = true;
-    //      Sentry.close();
-    //    } catch (RuntimeException e) {
-    //      getErrorHandler()
-    //          .error(
-    //              "An exception occurred while closing the Sentry connection",
-    //              e,
-    //              ErrorCode.CLOSE_FAILURE);
-    //    } finally {
-    //      SentryEnvironment.stopManagingThread();
-    //    }
+    this.closed = true;
   }
 
   @Override
